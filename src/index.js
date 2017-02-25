150a5308a8411039e488f4f9cf0caaf0ef9ee42a
naveedaheer <naveedaheer@gmail.com>
2017-02-25 12:49:03 +0500

    Redux Besic example in JavaScript

--- src/index.js
+++ src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import {Provider} from "react-redux"
+import {createStore} from "redux"
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const firstState = 2;
+
+// reducer
+function counter(state=firstState, action){
+   switch(action.type){
+    case "INC":
+    return state +10
+    break;
+    case "DEC":
+    return state-7
+    break;
+    default:
+    return state
+  }
+}
+
+let store = createStore(counter)
+
+store.subscribe(()=>{console.log("Updated",store.getState())})
+
+store.dispatch({type: "INC", val: 10})
+store.dispatch({type: "DEC", val: 1})
